bb45d0bea2bf04e2430268f5145cda0a070c94af
Miguel Chavez <mc036229@gmail.com>
2025-10-14 20:32:44 -0500

    arreglando los modelos

--- src/Reserva/Models/Reserva.php
+++ src/Reserva/Models/Reserva.php
@@ -5,29 +5,3 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Reserva extends Model
-{
-    use HasFactory;
-    protected $table = 'reservas';
-    protected $fillable = [
-        'evento_id', 'proveedor_id', 'fecha_reserva', 'estado'
-    ];
-
-    public function evento(): BelongsTo
-    {
-        return $this->belongsTo(\App\Src\Evento\Models\Evento::class, 'evento_id');
-    }
-
-    public function proveedor(): BelongsTo
-    {
-        return $this->belongsTo(\App\Src\Proveedor\Models\Proveedor::class, 'proveedor_id');
-    }
-
-    /**
-     * Enlaza la factory correcta para el modelo Reserva.
-     */
-    protected static function newFactory()
-    {
-        return \Database\Factories\ReservaFactory::new();
-    }
-}
